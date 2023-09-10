f264b499b6a126e9e15b146ec421d263675eeb3a
taka <toumyou463100@gmail.com>
2023-09-10 17:32:36 +0900

    scss反映

--- work/taka-workspace/shared-project/vue-project/my-vue-app/src/main.js
+++ work/taka-workspace/shared-project/vue-project/my-vue-app/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import router from './router' // router.js をインポート
-import '@/assets/main.css'
+import '@/assets/main.scss'
 
 createApp(App)
   .use(router) // Vue Routerをアプリケーションに統合
