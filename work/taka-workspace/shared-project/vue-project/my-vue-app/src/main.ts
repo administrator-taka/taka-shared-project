df7f2ab7d5bd6eff00c492ce5ab8649ed10c8551
taka <toumyou463100@gmail.com>
2023-09-10 18:55:18 +0900

    typescript導入（完）

--- work/taka-workspace/shared-project/vue-project/my-vue-app/src/main.ts
+++ work/taka-workspace/shared-project/vue-project/my-vue-app/src/main.ts
@@ -1,10 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import router from './router' // router.js をインポート
 import '@/assets/main.scss'
-
-createApp(App)
-  .use(router) // Vue Routerをアプリケーションに統合
-  .mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ work/taka-workspace/shared-project/vue-project/my-vue-app/src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import YoutubeHome from '@/components/youtube-service/YoutubeHome.vue'
+import TopComponent from '@/components/TopComponent.vue'
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'TopComponent',
+    component: TopComponent
+  },    {
+    path: '/youtubeHome',
+    name: 'YoutubeHome',
+    component: YoutubeHome
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
