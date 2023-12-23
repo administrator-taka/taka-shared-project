64c225f5884a55b8eebcbd616abf66515396390b
taka <toumyou463100@gmail.com>
2023-12-23 13:44:48 +0900

    コメント

--- work/taka-workspace/shared-project/vue-project/my-vue-app/vue.config.js
+++ work/taka-workspace/shared-project/vue-project/my-vue-app/vue.config.js
@@ -13,6 +13,7 @@ module.exports = defineConfig({
     port: 3000,
     proxy: {
       "/api": {
+        // TODO:vue側も環境変数から取得するように修正
         target: "http://localhost:8080",
         // target: "http://host.docker.internal:8080",
         changeOrigin: true,
