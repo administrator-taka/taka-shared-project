85badf14cc0c24cfe0561618dfa9d1e6dcbbc959
taka <toumyou463100@gmail.com>
2023-12-24 19:06:18 +0900

    ローカルでも動作できるか確認

--- work/taka-workspace/shared-project/django-project/myproject/myproject/settings.py
+++ work/taka-workspace/shared-project/django-project/myproject/myproject/settings.py
@@ -79,7 +79,9 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
+        # TODO:環境変数から取得するように修正、その他も同様
         'HOST': 'db',  # Docker 内の PostgreSQL サービスの名前
+        # 'HOST': 'localhost',
         'PORT': '5432',
     }
 }
