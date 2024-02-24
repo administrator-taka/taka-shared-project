4cef1dd3c1eaad0dc70b67bac4472a70d43d29bc
taka <toumyou463100@gmail.com>
2024-02-24 17:35:17 +0900

    db、update動作確認

--- work/taka-workspace/shared-project/django-project/myproject/myapp/views.py
+++ work/taka-workspace/shared-project/django-project/myproject/myapp/views.py
@@ -1,3 +1,4 @@
+from django.utils.crypto import get_random_string
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -7,7 +8,15 @@ def my_view(request):
     data = request.data
     print(data)
 
-    # Test モデルのすべてのレコードをシリアライザを使ってシリアライズして出力する
+    # idが"3"のTestモデルのインスタンスを取得
+    test_instance = Test.objects.get(id="3")
+
+    # パスワードをランダムな文字列に更新
+    new_password = get_random_string(length=12)  # 12文字のランダムな文字列を生成
+    test_instance.password = new_password
+    test_instance.save()
+
+    # 更新後のTestモデルの全レコードをシリアライズして出力
     tests = Test.objects.all()
     serializer = TestSerializer(tests, many=True)
     print(serializer.data)
