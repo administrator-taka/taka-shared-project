fbeeaec3e8ea24a99c04587b96a5f93982934174
taka <toumyou463100@gmail.com>
2024-02-24 14:40:34 +0900

    docker環境でもローカルのホストを受け付けるように修正

--- work/taka-workspace/shared-project/django-project/myproject/myproject/settings/development.py
+++ work/taka-workspace/shared-project/django-project/myproject/myproject/settings/development.py
@@ -3,4 +3,4 @@ from .base import *
 DATABASES['default'].update({
     'HOST': 'db',  # Docker 内の PostgreSQL サービスの名前
 })
-ALLOWED_HOSTS = ['host.docker.internal']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'host.docker.internal']
